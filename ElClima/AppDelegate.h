a976d9267e9a3050d21f60f6b95dbeb2615c2208
German Pereyra <german.f.pereyra@gmail.com>
2015-03-17 18:14:12 -0300

    Initial Commit

---
+++ ElClima/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ElClima
+//
+//  Created by German Pereyra on 3/17/15.
+//  Copyright (c) 2015 German Pereyra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ElClima/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ElClima
+//
+//  Created by German Pereyra on 3/17/15.
+//  Copyright (c) 2015 German Pereyra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
